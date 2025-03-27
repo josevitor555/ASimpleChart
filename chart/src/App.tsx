050af167f01e13034d05efb120478760f2ec4c32
josevitor555 <koservitor123@gmail.com>
2025-03-27 10:44:26 -0300

    27/03/2025

---
+++ chart/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './input.css'
+
+// Import the Example component
+import {ChartEaxample} from './components/Component';
+
+function App() {
+
+  return (
+    <>
+      <ChartEaxample />
+    </>
+  )
+}
+
+export default App;
---
+++ chart/src/components/Component.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+import { TrendingUp } from "lucide-react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+
+const chartData = [
+  { month: "Janeiro", desktop: 186, mobile: 80 },
+  { month: "Fevereiro", desktop: 305, mobile: 200 },
+  { month: "Março", desktop: 237, mobile: 120 },
+  { month: "Abril", desktop: 73, mobile: 190 },
+  { month: "Maio", desktop: 209, mobile: 130 },
+  { month: "Junho", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig
+
+export function ChartEaxample() {
+
+  return (
+    <>
+      <Card>
+      <CardHeader>
+        <CardTitle className='text-2xl font-extralight'> Gráfico de Área - Empilhado </CardTitle>
+        <CardDescription className='text-base font-extralight'> A mostrar o total de visitantes nos últimos 6 meses </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dot" />} />
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="var(--color-mobile)"
+              fillOpacity={0.4}
+              stroke="var(--color-mobile)"
+              stackId="a"
+            />
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="var(--color-desktop)"
+              fillOpacity={0.4}
+              stroke="var(--color-desktop)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-extralight text-2xl leading-none">
+              Tendência de Alta de 5,2% ao mês <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground"> Janeiro - Junho 2024 </div>
+            <Button className='mt-2 bg-gray-900 text-amber-50'> Mais detalhes </Button>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+    </>
+  )
+}
